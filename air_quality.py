4aed57359aaec1a86b24e7f53c53eb62cb7558e0
Patrick Larson <106538709+patrickslarson8@users.noreply.github.com>
2022-09-23 10:57:52 -0500

    Create air_quality.py
    Create description and todo list

---
+++ air_quality.py
@@ -0,0 +1,5 @@
+# This script captures data from the sensors and logs it.
+# Todo: 
+# 1. Capture data from all sensors
+# 2. Log data to prevent memory filling
+# 3. Create web dashboard for tracking
