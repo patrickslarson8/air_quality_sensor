24383847685e0370ed38105b80d3d9c59a6c8add
Patrick Larson <106538709+patrickslarson8@users.noreply.github.com>
2022-09-23 12:55:52 -0500

    Update air_quality.py
    Add airQuality class to store data
    Add delay to for sensor stabilization

--- air_quality.py
+++ air_quality.py
@@ -13,24 +13,40 @@ import adafruit_sgp40
 import adafruit_scd4x
 from digitalio import DigitalInOut, Direction, Pull
 from adafruit_pm25.i2c import PM25_I2C
-from adafruit_pm25.uart import PM25_UART
 
 # Set up IO
 # i2c = board.I2C()  # uses board.SCL and board.SDA # From scp and sgp, I think same as following cmd
 i2c = busio.I2C(board.SCL, board.SDA, frequency=100000)
 
 # Set up devices
+# sgp
 sgp = adafruit_sgp40(i2c)
+
+# scd
 scd4x = adafruit_scd4x.SCD4X(i2c)
+scd4x.start_periodic_measurement()
+
+#pm25
 reset_pin = DigitalInOut(board.G0)
 reset_pin.direction = Direction.OUTPUT
 reset_pin.value = False
 pm25 = PM25_I2C(i2c, reset_pin)
 
-# Set up variable to save data
-air_quality = []
+# Set up object to save data
+class airQuality:
+  pm10 = 0
+  pm25 = 0
+  co2 = 0
+  temp = 0
+  humid = 0
+  voc = 0
+
+# Stabilize readings
+print("Waiting for readings to stabilize")
+time.sleep(180) #pm25 requires 30 sec. SGP40 recommends several minutes before reading
 
 # Read and log sensors
 while True:
   # Read sensors
   # log sensors
+  time.sleep(1)
