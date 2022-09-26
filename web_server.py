a42351ee3d21f5317a1992bf594b8c18aa3ccc28
patrickslarson8 <pat.s.larson8@gmail.com>
2022-09-26 17:16:57 -0500

    Populate web_server.py with testing code

--- web_server.py
+++ web_server.py
@@ -3,14 +3,24 @@ import altair as alt
 
 #todo
 #data = read from database
-base = alt.Chart(data).transform_fold(
-    ['temperature', 'humidity', 'carbon dioxide, VOCs, particulate 1.0, particulate 2.5']
-)
-line = base.mark_line().encode(
-    x='date:T',
-    y=alt.Y('value:Q', axis=alt.Axis(title='air quality')),
-    color='key:N',
-    tooltip=['yearmonthdate(date)']
-).properties(width=700)
 
-line
+
+# base = alt.Chart(data).transform_fold(
+#     ['temperature', 'humidity', 'carbon dioxide, VOCs, particulate 1.0, particulate 2.5']
+# )
+# line = base.mark_line().encode(
+#     x='date:T',
+#     y=alt.Y('value:Q', axis=alt.Axis(title='air quality')),
+#     color='key:N',
+#     tooltip=['yearmonthdate(date)']
+# ).properties(width=700)
+#
+# line
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return 'Hello World'
+
+if __name__ == '__main__':
+    app.run(debug=True, port=80, host='0.0.0.0')
