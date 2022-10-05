8a4d9394aae56ca5af7ffba1bce411b516effc4d
Pat L <pat.s.larson8@gmail.com>
2022-10-05 12:23:05 -0500

    Add template .html for Flask

--- web_server.py
+++ web_server.py
@@ -24,7 +24,21 @@ line
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'Hello World'
+    # total confirmed cases globally
+    total_all_confirmed = total_confirmed[total_confirmed.columns[-1]].sum()
+    total_all_recovered = total_recovered[total_recovered.columns[-1]].sum()
+    total_all_deaths = total_death[total_death.columns[-1]].sum()
+    #ploting
+    plot_global_cases_per_country = plotly_plot.plotly_global_cases_per_country(
+        final_df)
+    plot_global_time_series = plotly_plot.plotly_global_timeseries(
+        timeseries_final)
+    plot_geo_analysis = plotly_plot.plotly_geo_analysis(final_df)
+    context = {"total_all_confirmed": total_all_confirmed,
+               "total_all_recovered": total_all_recovered, "total_all_deaths": total_all_deaths,
+            'plot_global_cases_per_country': plot_global_cases_per_country,
+            'plot_global_time_series': plot_global_time_series,'plot_geo_analysis': plot_geo_analysis}
+    return render_template('index.html', context=context)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
