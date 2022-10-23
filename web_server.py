cad6321c1d9cf6488264cc123df1ffd76a55d046
Patrick Larson <106538709+patrickslarson8@users.noreply.github.com>
2022-10-23 12:10:00 -0500

    Update web_server.py
    Clean up unused code

--- web_server.py
+++ web_server.py
@@ -6,10 +6,6 @@ import pandas as pd
 # Create connection to database
 conn = lite.connect('sensorsData.db', check_same_thread=False)
 
-# Get information from database into a dataframe
-selectStatement = "SELECT * FROM SENSORS_data;"
-data = pd.read_sql_query(selectStatement, conn)
-
 def get_top_data():
      cur = conn.cursor()
      cur.execute("SELECT * FROM SENSORS_data ORDER BY timestamp DESC LIMIT 10;")
@@ -17,21 +13,6 @@ def get_top_data():
      rows = pd.read_sql("SELECT * FROM SENSORS_data ORDER BY timestamp DESC LIMIT 1000;",
      conn)
      return rows
-
-## For reference
-def altair_global_timeseries(data):
-    base = alt.Chart(data).transform_fold(
-    ['daily new cases', 'daily new recovered', 'daily new deaths']
-    )
-    line = base.mark_line().encode(
-    x='date:T',
-    y=alt.Y('value:Q', axis=alt.Axis(title='# of cases')),
-    color='key:N',
-    tooltip=['yearmonthdate(date)','daily new cases',
-      'daily new recovered', 'daily new deaths']
-    ).properties(width=700)
-    chart_json = line.to_json()
-    return chart_json
 
 def altair_temperature(df):
      base = alt.Chart(df)
