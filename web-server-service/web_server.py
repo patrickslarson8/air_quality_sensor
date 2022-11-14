709c8a37991fd7814ab3ef8dae21900f971fe14c
Patrick Larson <pat.s.larson8@gmail.com>
2022-11-14 12:09:16 -0600

    Make charts stack vertically

--- web-server-service/web_server.py
+++ web-server-service/web_server.py
@@ -7,7 +7,11 @@ import pandas as pd
 conn = lite.connect('../air-quality-service/database.db', check_same_thread=False)
 
 def get_top_data_as_df():
-     rows = pd.read_sql("SELECT * FROM sensor_table ORDER BY timestamp DESC LIMIT 10;",conn)
+     rows = pd.read_sql("SELECT * FROM sensor_table ORDER BY timestamp DESC LIMIT 100;",conn)
+     return rows
+
+def get_all_as_df():
+     rows = pd.read_sql("SELECT * FROM sensor_table ORDER BY timestamp DESC;",conn)
      return rows
 
 def get_every_nth_row_as_df(n):
@@ -64,7 +68,7 @@ def altair_particulate_25_10(df):
      )
 
      line_B = base.mark_line(color='#F18727').encode(
-     alt.Y('pm25:Q', axis=alt.Axis(titleColor='#F18727'))
+     alt.Y('pm10:Q', axis=alt.Axis(titleColor='#F18727'))
      )
 
      base = alt.layer(line_A, line_B).resolve_scale(y='shared')
@@ -82,17 +86,12 @@ def build_context(df):
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-     #currently broken TODO:
-     rows = get_top_data_as_df()
-     formatted_information = f" Date and time {rows[0][0]}\n Temperature: {rows[0][1]}\n Humidity {rows[0][2]}\n CO2: {rows[0][3]}\n VOC: {rows[0][4]}\n PM10: {rows[0][5]}\n PM25: {rows[0][6]}\n"
-     return formatted_information
-
-@app.route('/charts')
 def template():
      rows = get_top_data_as_df()
-     context = alt.vconcat(altair_temp_and_humid(rows), altair_voc_co2(rows))
-     context = alt.vconcat(context, altair_particulate_25_10(rows)).to_json()
+     chart = alt.vconcat(altair_temp_and_humid(rows), altair_voc_co2(rows))
+     chart = alt.vconcat(chart, altair_particulate_25_10(rows))
+     chart.properties(width='container',height='container')
+     context = chart.to_json()
      return render_template('index.html', context = context)
 
 if __name__ == '__main__':
